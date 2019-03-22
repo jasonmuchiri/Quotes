38e9a2ebb5f8ba24166696339817bec17f77fa8e
jasonmuchiri <jasonmkinyua@gmail.com>
2019-03-22 17:36:33 +0300

    add quote to the project

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote'
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+  quotes = [
+      new Quote('life is short, smile while you still have teeth'),
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

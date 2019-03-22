5fde15b98d6c2b0f79072a58763b63d4fa4b9764
jasonmuchiri <jasonmkinyua@gmail.com>
2019-03-22 16:08:08 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  quote = 'life is short, smile while you still have teeth';
 }

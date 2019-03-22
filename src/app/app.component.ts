5d091cb581151eb245d960f0b0476eb05a67bbe2
jasonmuchiri <jasonmkinyua@gmail.com>
2019-03-22 17:39:00 +0300

    add all changes in functionality made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Quote} from './quote'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quote = 'life is short, smile while you still have teeth';
+  quotes = [
+    new Quote(1,'life is short, smile while you still have teeth'),
+  ]
 }
